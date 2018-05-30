341271acd09d4c3b9f68b0df1d68a5174598249a
langdag <lucky10cents@gmail.com>
2018-05-30 15:44:41 +0300

    Added CRUD actions to the post controller

--- app/controllers/api/post_controller.rb
+++ app/controllers/api/post_controller.rb
@@ -1,6 +1,28 @@
 module Api
 	class PostController < BaseController
 		def index
-		end 
+		  render json: Post.all
+		end
+
+		def create
+		  post = Post.create(post_params)
+		  render json: post
+		end
+
+		def destroy
+		  Post.destroy(params[:id])
+		end
+
+		def update
+		  post = Post.find(params[:id])
+		  post.update_attributes(post_params)
+		  render json: post
+		end
+
+		private
+		
+		def post_params
+		  params.require(:post).permit(:id, :title, :author, :description, :image)
+		end
 	end
 end
